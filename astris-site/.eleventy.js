5b8daaec986fb01eaa42ae0768fc2311bbc11e85
LidPav <pavlovic_lidija@yahoo.com>
2026-02-03 23:01:48 +0100

    Update .eleventy.js

--- astris-site/.eleventy.js
+++ astris-site/.eleventy.js
@@ -5,3 +5,21 @@ module.exports = function (eleventyConfig) {
     pathPrefix: "/astris-trio-website/"
   };
 };
+
+// .eleventy.js
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+  // Deutsches Datumsformat, Zeitzone München/Berlin
+  eleventyConfig.addFilter("deDate", (dateObj, fmt = "d. LLLL yyyy", zone = "Europe/Berlin") => {
+    if(!dateObj) return "";
+    return DateTime.fromJSDate(dateObj, { zone: "utc" })
+      .setZone(zone)
+      .setLocale("de")
+      .toFormat(fmt);
+  });
+
+  return {
+    // optional: falls du UTC beibehalten willst, nichts ändern
+  };
+};
